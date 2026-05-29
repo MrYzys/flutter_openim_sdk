616c1c87ce9cb164efb41e182608c1379e075b58
河川 <hechuan@mbp14>
2026-05-29 21:35:45 +0800

    更新日志打印

--- lib/src/manager/im_manager.dart
+++ lib/src/manager/im_manager.dart
@@ -39,7 +39,7 @@ class IMManager {
   void _addNativeCallback(NativeBridge bridge) {
     bridge.setMethodCallHandler((call) {
       try {
-        Logger.print("Flutter :  ${call.method} ${call.arguments['type']} ${call.arguments['data']} $error $stackTrace");
+        Logger.print("Flutter :  ${call.method} ${call.arguments?['type']??''} ${call.arguments?['data']??''}");
         if (_handleNewStyleEvent(call)) {
           return Future.value(null);
         }
